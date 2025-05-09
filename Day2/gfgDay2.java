42e57109e033151e2bf6cba24a461a4743ff9c3e
Tanmay Bharambe <bharambetr2002@gmail.com>
2025-05-09 23:37:18 +0530

    Day 1 & 2 added

--- Day2/gfgDay2.java
+++ Day2/gfgDay2.java
@@ -1,5 +1,22 @@
 package Day2;
 
 public class gfgDay2 {
-    
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 0, 3, 0, 4, 5, 0 };
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+
+                count++;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
 }
--- Day2/leetcode2965.java
+++ Day2/leetcode2965.java
@@ -1,5 +1,33 @@
 package Day2;
 
+import java.util.Arrays;
+
 public class leetcode2965 {
-    
+    public static void main(String[] args) {
+        int[][] grid = { { 1, 3 }, { 2, 2 } };
+        int[] result = findDuplicateAndMissing(grid);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] findDuplicateAndMissing(int[][] grid) {
+        int n = grid.length;
+        int size = n * n;
+        int[] freq = new int[size + 1];
+        int repeated = -1, sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int num = grid[i][j];
+                sum += num;
+
+                if (freq[num] == 1) {
+                    repeated = num;
+                }
+                freq[num]++;
+            }
+        }
+        int expectedSum = (size * (size + 1)) / 2;
+        int missing = expectedSum - (sum - repeated);
+
+        return new int[] { repeated, missing };
+    }
 }
